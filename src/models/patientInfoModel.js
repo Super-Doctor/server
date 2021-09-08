1c3a00026832445dd4c0458b9d4532e8f44fb40f
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-09-08 12:50:24 +0300

    update on patient model

--- src/models/patientInfoModel.js
+++ src/models/patientInfoModel.js
@@ -1,29 +1,27 @@
 const patientInfoModel=(sequelize,DataTypes)=>sequelize.define('patientInfo',{
-    id: {
+    patientId: {
         type:DataTypes.INTEGER,
         allowNull:false,
         primaryKey: true
-
       },
-       name: {
-      type:DataTypes.STRING,
-      allowNull:false,
-    },
-    
     
       address: {
         type:DataTypes.STRING,
         allowNull:false,
       },
+
       phoneNumber: {
         type:DataTypes.INTEGER,
         allowNull:false,
       },
-      patientsHistory: {
-        type:DataTypes.STRING,
+
+      secondaryNumber: {
+        type:DataTypes.INTEGER,
         allowNull:false,
       },
 
-   
+      medicalHistory : {
+        type : DataTypes.STRING,
+      }
   });
   module.exports = patientInfoModel;
