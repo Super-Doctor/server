f1dd386a5f0cc61326b4ddfb5478e2adddba2261
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-09-08 20:20:54 +0300

    solvong routs issues

--- src/routes/roles-route.js
+++ src/routes/roles-route.js
@@ -2,18 +2,17 @@
 
 const express = require('express');
 const authRouter = express.Router();
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcrypt');
 const { doctors } = require('../models/doctor-model');
-const {Role} = require('../models/index')
+const { Role } = require('../models/index')
 const basicAuth = require('../middlewares/basic-auth')
 const bearerAuth = require('../middlewares/bearer-auth')
 
-authRouter.post('/signUp', async (req, res, next) => {
+authRouter.post('/addRole', async (req, res, next) => {
   try {
     let roleRecord = await Role.create(req.body);
     const output = {
-        Role: roleRecord,
-      token: roleRecord.token
+      Role: roleRecord,
     };
     res.status(200).json(output);
   } catch (e) {
@@ -21,17 +20,24 @@ authRouter.post('/signUp', async (req, res, next) => {
   }
 });
 
-authRouter.post('/signIn', basicAuth, (req, res, next) => {
-  const role = {
-    Role: req.Role,
-    token: req.Role.token
-  };
-  res.status(200).json(role);
-});
+// authRouter.post('/signIn', basicAuth, (req, res, next) => {
+//   const role = {
+//     Role: req.Role,
+//     token: req.Role.token
+//   };
+//   res.status(200).json(role);
+// });
 
-authRouter.get('/users', bearerAuth, async (req, res, next) => {
-  const user = await users.findAll({});
-  const list = user.map(user => user.username);
+authRouter.get('/roles', async (req, res, next) => {
+  const roles = await Role.get({});
+  const list = roles.map(role => {
+    return (
+      role.role,
+      role.capabilities
+      )
+
+  });
+
   res.status(200).json(list);
 });
 authRouter.get('/', async (req, res, next) => {
