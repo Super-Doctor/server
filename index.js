16384424a4f4ebeeda927c26b9b6a82ad816adef
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-09-15 22:21:28 +0300

    fixing chat

--- index.js
+++ index.js
@@ -2,9 +2,4 @@
 
 require('dotenv').config();
 const server = require('./src/server');
-const {db} = require('./src/models/index');
 
-db.sync().then(()=>{
-    server.start(process.env.PORT || 3001)
-    // chatServer.startup(process.env.PORT||3002);
-})
